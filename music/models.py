1d95cd75f84a10d710b4e9b866f097ec8bc9c393
Bastiaan <laaken@un.org>
2016-03-29 17:03:25 +0200

    First Form created

--- music/models.py
+++ music/models.py
@@ -8,9 +8,16 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.artist + " - " + self.album_title
+
 
 # songs: title, artist
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title
